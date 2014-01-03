908c9183ea53bedf24251c0fcfd6be8fdb509faf
Michał Bachowski <michal@bachowski.pl>
2014-01-03 14:30:06 +0100

    Added doctests for pyqp.reducers; Added 'doctest' command to makefile

--- src/pyqp/reducers.py
+++ src/pyqp/reducers.py
@@ -7,10 +7,38 @@ import math
 # in python there are built in: sum, min and max
 
 def last(values):
+    """Returns last value from given list
+
+    >>> last([1,2,3])
+    3
+
+    >>> last()
+    Traceback (most recent call last):
+    ...
+    TypeError: last() takes exactly 1 argument (0 given)
+    """
     return values[-1]
 
 
 def nvl(values):
+    """Returns first non-None value from given list
+
+    >>> nvl([1,2,3])
+    1
+
+    >>> nvl([None, 1, None, 2])
+    1
+
+    >>> nvl([None])
+    Traceback (most recent call last):
+    ...
+    ValueError: Not None value not found
+
+    >>> nvl()
+    Traceback (most recent call last):
+    ...
+    TypeError: nvl() takes exactly 1 argument (0 given)
+    """
     for val in values:
         if val is not None:
             return val
@@ -18,11 +46,52 @@ def nvl(values):
 
 
 class Percentile(object):
+    """Calculates requested percentile value for given list of values
+
+    >>> Percentile(0.25)([1,2,3,4])
+    1.75
+
+    >>> Percentile(0.25)([1,2,3,4,5])
+    2
+
+    >>> Percentile(0.5)([1,2,3])
+    2
+
+    >>> Percentile(0.5)([1,2,3,4])
+    2.5
+    """
 
     def __init__(self, percentile):
+        """Object initialization
+
+        >>> Percentile(1)    #doctest: +ELLIPSIS
+        <reducers.Percentile object at 0x...>
+
+        >>> Percentile()
+        Traceback (most recent call last):
+        ...
+        TypeError: __init__() takes exactly 2 arguments (1 given)
+        """
         self._percentile = percentile
 
     def __call__(self, values):
+        """Find the percentile of a list of values.
+
+        @param    values -- a list of values. Would be sorted internally
+        @type     values -- list
+        @return   float  -- the percentile of the values
+
+        >>> Percentile(0.25)([1,2,3,4,5])
+        2
+
+        >>> Percentile(0.5)([1, 2, 3])
+        2
+
+        >>> Percentile(0.75)()
+        Traceback (most recent call last):
+        ...
+        TypeError: __call__() takes exactly 2 arguments (1 given)
+        """
         sorts = sorted(values)
         key = (len(values)-1) * self._percentile
         key_floor = math.floor(key)
@@ -45,8 +114,27 @@ except ImportError:
 
 
     def mean(values):
+        """Calculates mean (average) for given numbers
+
+        >>> mean([1,2,3])
+        2.0
+        >>> mean()
+        Traceback (most recent call last):
+        ...
+        TypeError: mean() takes exactly 1 argument (0 given)
+        """
         return sum(values) / len(values)
 
 
     def mode(values):
+        """Calculates mode (most common value) of given discrete values
+
+        >>> mode([1,1,2,3])
+        1
+
+        >>> mode()
+        Traceback (most recent call last):
+        ...
+        TypeError: mode() takes exactly 1 argument (0 given)
+        """
         return max(set(values), key=values.count)
