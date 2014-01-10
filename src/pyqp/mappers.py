f38341ce511d93375afea84f1bee9d833f773d6d
Michał Bachowski <michal@bachowski.pl>
2014-01-10 11:24:41 +0100

    Refactored pyqp.mappers.(alias, select, exclude) to allow setting which key to check (by default '2' - column)

--- src/pyqp/mappers.py
+++ src/pyqp/mappers.py
@@ -3,6 +3,16 @@
 from operator import itemgetter
 from functools import wraps
 
+class Keys(object):
+    TABLE_NAME = 0
+    ROW_ID = 1
+    COLUMN_NAME = 2
+    VALUE = 3
+
+def _alter_tuple(row, index, value):
+    tmp = list(row)
+    tmp[index] = value
+    return tuple(tmp)
 
 def dict_row(primary_key_columns):
     _pk = itemgetter(*primary_key_columns)
@@ -13,41 +23,37 @@ def dict_row(primary_key_columns):
             yield (event_name, pk, column, value)
     return _map
 
-
 def single_value(event_name, data):
     yield (data[0], data[1], data[2], data[3])
-
 
 def values_list(event_name, data):
     for value in data:
         yield (value[0], value[1], value[2], value[3])
 
-
-def alias(decorated, aliases):
+def alias(decorated, aliases, key=2):
     @wraps(alias)
     def _map(event_name, iterable):
         for row in decorated(event_name, iterable):
             yield row
-            if row[2] not in aliases:
+            val = row[key]
+            if val not in aliases:
                 continue
-            for alias in aliases[row[2]]:
-                yield (row[0], row[1], alias, row[3])
+            for alias in aliases[val]:
+                yield _alter_tuple(row, key, alias)
     return _map
 
-
-def select(decorated, allowed):
+def select(decorated, allowed, key=2):
     @wraps(select)
     def _map(event_name, iterable):
         for row in decorated(event_name, iterable):
-            if row[2] in allowed:
+            if row[key] in allowed:
                 yield row
     return _map
 
-
-def exclude(decorated, excluded):
+def exclude(decorated, excluded, key=2):
     @wraps(exclude)
     def _map(event_name, iterable):
         for row in decorated(event_name, iterable):
-            if row[2] not in excluded:
+            if row[key] not in excluded:
                 yield row
     return _map
