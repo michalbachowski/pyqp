0f86989dcfe880394a5340a2cd0f49ed07651723
Michał Bachowski <michal@bachowski.pl>
2014-01-14 13:47:45 +0100

    Added doctests for methods in module pyqp.columns.factory; Fixed some column factories

--- src/pyqp/columns/factory.py
+++ src/pyqp/columns/factory.py
@@ -17,7 +17,19 @@ def aggregate_factory(*factories):
     return _factory
 
 def column_instance_factory(config):
-    # so maybe config is valid Column instance?
+    """Creates new instance out of existing Column instance
+
+    >>> c = Column('foo')
+    >>> c1 = column_instance_factory(c)
+    >>> c == c1
+    False
+    >>> c1.name == c.name
+    True
+    >>> column_instance_factory('f')
+    Traceback (most recent call last):
+    ...
+    TypeError: Expected config to be instance of pyqp.columns.Column, got <type 'str'>
+    """
     try:
         config.name
         config.desc
@@ -27,28 +39,62 @@ def column_instance_factory(config):
         config.__copy__
         config.__str__
     except AttributeError:
-        raise TypeError('Expected config to be instance of "Column", got %s' % \
+        raise TypeError('Expected config to be instance of pyqp.columns.Column, got %s' % \
                                                                 type(config))
     else:
         return copy(config)
 
 def dict_factory(config):
+    """Creates new instance of Column using given dict
+
+    >>> c = dict_factory({'name': 'ggg'})
+    >>> c.name
+    'ggg'
+    >>> dict_factory('f')
+    Traceback (most recent call last):
+    ...
+    TypeError: Expected config to be a dict, got <type 'str'>
+    """
     try:
         return Column(**config)
     except TypeError:
-        raise TypeError('Expected config to a dict, got %s' % type(config))
+        raise TypeError('Expected config to be a dict, got %s' % type(config))
 
 def tuple_factory(config):
+    """Creates new instance of Column using given tuple / list
+
+    >>> c = tuple_factory(('ggg',))
+    >>> c.name
+    'ggg'
+    >>> tuple_factory('f')
+    Traceback (most recent call last):
+    ...
+    TypeError: Expected config to be a tuple, got <type 'str'>
+    """
     try:
+        # ensure we work with iterable (list or tuple), not str
+        config.__iter__
         return Column(*config)
-    except TypeError:
-        raise TypeError('Expected config to a tuple, got %s' % type(config))
+    except (TypeError, AttributeError):
+        raise TypeError('Expected config to be a tuple, got %s' % type(config))
 
 def string_factory(config):
+    """Creates new instance of Column using given string
+
+    >>> c = string_factory('ggg')
+    >>> c.name
+    'ggg'
+    >>> string_factory(['123'])
+    Traceback (most recent call last):
+    ...
+    TypeError: Expected config to be a string, got <type 'list'>
+    """
     try:
+        # ensure we work with str
+        config.strip
         return Column(config)
-    except TypeError:
-        raise TypeError('Expected config to a string, got %s' % type(config))
+    except (TypeError, AttributeError):
+        raise TypeError('Expected config to be a string, got %s' % type(config))
 
 
 default_column_factory = aggregate_factory(column_instance_factory, \
