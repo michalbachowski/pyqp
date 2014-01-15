05ea27073300ff75265a2f0c273dc1844c5951f6
Michał Bachowski <michal@bachowski.pl>
2014-01-15 15:54:43 +0100

    Added doc-tests to pyqp.configurators

--- src/pyqp/configurators/__init__.py
+++ src/pyqp/configurators/__init__.py
@@ -4,6 +4,14 @@ from itertools import chain
 from functools import wraps
 
 def filtered_configurator(base_configurator, filter_func):
+    """Factory method that creates configuration and applies given filter to received output
+
+    >>> f = filtered_configurator(lambda c: [(1, 2, 3)], lambda a, b, c: [(a+10, b+10, c+10)])
+    >>> f # doctest: +ELLIPSIS
+    <function filtered_configurator at 0x...>
+    >>> list(f('fooo'))
+    [(11, 12, 13)]
+    """
 
     def _apply_filter(item):
         return filter_func(*item)
@@ -15,9 +23,24 @@ def filtered_configurator(base_configurator, filter_func):
     return _configurator
 
 def simple_dict_config(config):
+    """Factory method that creates configuration for table from given dictionary
+
+    >>> list(simple_dict_config({'table': 'foo', 'dumper': 'bar'}))
+    [('foo', 'bar', {'table': 'foo', 'dumper': 'bar'})]
+    >>> list(simple_dict_config({}))
+    [(None, None, {})]
+    """
     yield (config.get('table'), config.get('dumper'), config)
 
 def list_of_configs(base_configurator):
+    """Factory method that creates configuration for table from given list of configurations
+
+    >>> f = list_of_configs(simple_dict_config)
+    >>> f # doctest: +ELLIPSIS
+    <function list_of_configs at 0x...>
+    >>> list(f([{'table': 'foo', 'dumper': 'bar'}]))
+    [('foo', 'bar', {'table': 'foo', 'dumper': 'bar'})]
+    """
     @wraps(list_of_configs)
     def _configurator(config):
         return chain.from_iterable(map(base_configurator, config))
