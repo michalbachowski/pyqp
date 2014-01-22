10903d2177fb1c9619854608838bd2df54779f09
Michał Bachowski <michal@bachowski.pl>
2014-01-22 10:41:07 +0100

    Added doctests for pyqp.mappers; Refactored pyqp.mappers.dict_row to accept more input arguments instead of forcing users to pass tuple

--- src/pyqp/mappers/__init__.py
+++ src/pyqp/mappers/__init__.py
@@ -12,14 +12,18 @@ class Keys(object):
     VALUE = 3
 
 def filtered_mapper(base_mapper, filter_func):
-    """Dumps given table using predefined base_mapper.
-    Then given filter is applied to received output
+    """First filters given data using predefined base_mapper,
+    then given filter is applied to received output
 
     @param  base_mapper -- base mapper to perform table map
     @type   base_mapper -- callable
     @param  filter_func -- filter to be applied to base_mapper output
     @type   filter_func -- callable
-    @return type
+    @return callable -- callable that accepts one argument: input data
+
+    >>> m = filtered_mapper(lambda e, x: [x], lambda x: [x+1])
+    >>> list(m('event_name', 2))
+    [3]
     """
 
     @wraps(filtered_mapper)
@@ -28,7 +32,22 @@ def filtered_mapper(base_mapper, filter_func):
                                                                         data)))
     return _map
 
-def dict_row(primary_key_columns):
+def dict_row(*primary_key_columns):
+    """Maps table data given as dict to list of tuples
+    [(table_name, pk, column, value), ...]
+    Event name is treated as table name and primary key is composed
+    from values from one or more columns.
+
+    @param  *primary_key_columns -- one or more columns names that composes primary key
+    @type   *primary_key_columns -- str
+    @return type
+
+    >>> from operator import itemgetter
+    >>> m = dict_row('a')
+    >>> ig = itemgetter(2)
+    >>> sorted(list(m('event_name', {'a': 1, 'b': 3})), key=ig) == sorted([('event_name', 1, 'a', 1), ('event_name', 1, 'b', 3)], key=ig)
+    True
+    """
     _pk = itemgetter(*primary_key_columns)
     @wraps(dict_row)
     def _map(event_name, data):
