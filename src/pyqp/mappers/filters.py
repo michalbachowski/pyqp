fe32d7e355f8540966279d55804bd4b0e822ef6b
Michał Bachowski <michal@bachowski.pl>
2014-01-22 09:30:36 +0100

    Added doctests for pyqp.mappers

--- src/pyqp/mappers/filters.py
+++ src/pyqp/mappers/filters.py
@@ -4,6 +4,15 @@ from functools import wraps
 from itertools import chain
 
 def aggregate_filter(*filters):
+    """Filter that aggregates other filters and uses them to filter value
+
+    >>> f = aggregate_filter(lambda x: [x*2])
+    >>> list(f(2))
+    [4]
+    >>> f = aggregate_filter(lambda x: [x, x*2], lambda x: [x+10, x+20])
+    >>> list(f(2))
+    [12, 22, 14, 24]
+    """
 
     def _apply_filters(output, filter_func):
         return chain.from_iterable(map(filter_func, output))
@@ -14,11 +23,32 @@ def aggregate_filter(*filters):
     return _filter
 
 def _alter_tuple(cell, index, value):
+    """Alters given tuple: changes value from given index to given new value.
+    It casts tuple to list, puts new value and returns tuple"""
     tmp = list(cell)
     tmp[index] = value
     return tuple(tmp)
 
 def alias(*args, **aliases):
+    """Filter returns all given tuples plus copies of those
+    having value under given key (table name, pk, column name or value)
+    found in alias list. You may put as many aliases as you want.
+
+    VALUES ARE PASSED AS REFERENCE!
+
+    >>> f = alias(a=['b'])
+    >>> list(f((1, 2, 3, 4)))
+    [(1, 2, 3, 4)]
+    >>> list(f((1, 2, 'a', 4)))
+    [(1, 2, 'a', 4), (1, 2, 'b', 4)]
+    >>> f= alias(1, a=['b'])
+    >>> list(f((1, 2, 3, 4)))
+    [(1, 2, 3, 4)]
+    >>> list(f((1, 2, 'a', 4)))
+    [(1, 2, 'a', 4)]
+    >>> list(f((1, 'a', 3, 4)))
+    [(1, 'a', 3, 4), (1, 'b', 3, 4)]
+    """
     try:
         key = args[0]
     except IndexError:
@@ -34,6 +64,24 @@ def alias(*args, **aliases):
     return _filter
 
 def select(*allowed, **kwargs):
+    """Filter returns only tuples having value under given key
+    (table name, pk, column name or value) allowed.
+
+
+    >>> f = select('a', 'b')
+    >>> list(f((1, 2, 3, 4)))
+    []
+    >>> list(f((1, 2, 'a', 4)))
+    [(1, 2, 'a', 4)]
+    >>> f= select('a', 'b', key=1)
+    >>> list(f((1, 2, 3, 4)))
+    []
+    >>> list(f((1, 2, 'a', 4)))
+    []
+    >>> list(f((1, 'a', 3, 4)))
+    [(1, 'a', 3, 4)]
+    """
+
     key = kwargs.get('key', 2)
     @wraps(select)
     def _filter(cell):
@@ -42,6 +90,23 @@ def select(*allowed, **kwargs):
     return _filter
 
 def exclude(*excluded, **kwargs):
+    """Filter returns all tuples except those having value under given key
+    (table name, pk, column name or value) disallowed.
+
+
+    >>> f = exclude('a', 'b')
+    >>> list(f((1, 2, 3, 4)))
+    [(1, 2, 3, 4)]
+    >>> list(f((1, 2, 'a', 4)))
+    []
+    >>> f= exclude('a', 'b', key=1)
+    >>> list(f((1, 2, 3, 4)))
+    [(1, 2, 3, 4)]
+    >>> list(f((1, 2, 'a', 4)))
+    [(1, 2, 'a', 4)]
+    >>> list(f((1, 'a', 3, 4)))
+    []
+    """
     key = kwargs.get('key', 2)
     @wraps(exclude)
     def _filter(cell):
