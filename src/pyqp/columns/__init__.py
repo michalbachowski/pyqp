850b3f8207afa03ab1f02ac4749aca981a5478c5
Michał Bachowski <michal@bachowski.pl>
2014-02-13 09:10:21 +0100

    Removed read-only properties in favour of read-write public attributes

--- src/pyqp/columns/__init__.py
+++ src/pyqp/columns/__init__.py
@@ -8,25 +8,13 @@ class Column(object):
 
     def __init__(self, name, reducer=last, handler_factory=LastValue,
                  desc=None, type_name='str', default_value=0):
-        self._name = name
-        self._desc = desc if desc is not None else name
-        self._type_name = type_name
+        self.name = name
+        self.desc = desc if desc is not None else name
+        self.type_name = type_name
         self._reducer = reducer
         self._default_value = default_value
         self._handler_factory = handler_factory
         self._handler = self._handler_factory()
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def desc(self):
-        return self._desc
-
-    @property
-    def type_name(self):
-        return self._type_name
 
     def duplicate(self):
         """Makes duplicate of itself *without any data*
