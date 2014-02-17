5730f17f240590a78ee5b9cc3f8c8caa855eb048
Michał Bachowski <michal@bachowski.pl>
2014-02-17 17:19:31 +0100

    Refactored pyqp.tables.Table to expect list of instances of classes that subclass pyqp.columns.Column (contain attribute 'name' and method 'duplicate' in particular)

--- src/pyqp/tables/__init__.py
+++ src/pyqp/tables/__init__.py
@@ -2,25 +2,18 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict, OrderedDict
 from operator import methodcaller
-from pyqp.columns.factory import default_column_factory
 
 
 class Table(object):
 
-    def __init__(self, name, columns, column_factory=default_column_factory, \
-                                                            *args, **kwargs):
+    def __init__(self, name, columns):
         self.name = name
-        self._columns_conf = columns
-        self._column_factory = column_factory
-        self.columns = list(self._create_columns_list())
+        self.columns = columns
         self._rows = defaultdict(self._init_row)
         self._values_extractor = methodcaller('values')
 
-    def _create_columns_list(self):
-        return map(self._column_factory, self._columns_conf)
-
     def _init_row(self):
-        return OrderedDict((c.name, c) for c in self._create_columns_list())
+        return OrderedDict((c.name, c.duplicate()) for c in self.columns)
 
     def add_value(self, row, column, value):
         self._rows[row][column].append(value)
