0b4a5a41a907a5a32d73733979cfc381f9f42d82
Michał Bachowski <michal@bachowski.pl>
2014-03-14 08:56:55 +0100

    Reorganized imports in example/test.py

--- example/test.py
+++ example/test.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pyqp import Column, Table
+from pyqp.reducers import mean
+from pyqp.aggregate import Accumulate
+
+from pyqp.event_dispatcher import Dispatcher
+from pyqp.mappers import dict_row, single_value, values_list, filtered_mapper, Keys
+from pyqp.mappers.filters import aggregate_filter as af2, alias, exclude
+from pyqp.dumpers import csv_dumper, filtered_dumper
+from pyqp.dumpers.filters import aggregate_filter as af, prepend, write_to_stdout
+
 from pyqp.configurators import simple_dict_config, list_of_configs, filtered_configurator
 from pyqp.configurators.columns import default_column_factory
 from pyqp.configurators.filters import aggregate_filter, make_forwardable, \
                                                             set_default_dumper
-from pyqp.mappers import dict_row, single_value, values_list, filtered_mapper, Keys
-from pyqp.mappers.filters import aggregate_filter as af2, alias, exclude
-from pyqp import Column, Table
-from pyqp.reducers import mean
-from pyqp.aggregate import Accumulate
-from pyqp.event_dispatcher import Dispatcher
-from pyqp.dumpers import csv_dumper, filtered_dumper
-from pyqp.dumpers.filters import aggregate_filter as af, prepend, write_to_stdout
 
 mappers = {
     'test': filtered_mapper(
