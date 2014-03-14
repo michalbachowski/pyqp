50c6cb24eb6a8bfa11033aed2ef57fc045ac6890
Michał Bachowski <michal@bachowski.pl>
2014-03-14 08:56:36 +0100

    Allow to import Table and Column classes right from pyqp module without specyfing sub-module name (pyqp.tables and pyqp.column respectively)

--- example/test.py
+++ example/test.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pyqp.column import Column
 from pyqp.configurators import simple_dict_config, list_of_configs, filtered_configurator
 from pyqp.configurators.columns import default_column_factory
 from pyqp.configurators.filters import aggregate_filter, make_forwardable, \
                                                             set_default_dumper
-from pyqp.tables import Table
 from pyqp.mappers import dict_row, single_value, values_list, filtered_mapper, Keys
 from pyqp.mappers.filters import aggregate_filter as af2, alias, exclude
+from pyqp import Column, Table
 from pyqp.reducers import mean
 from pyqp.aggregate import Accumulate
 from pyqp.event_dispatcher import Dispatcher
