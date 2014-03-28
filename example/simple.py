09c272262c38cdc7f543382778061642317f905f
Michał Bachowski <michal@bachowski.pl>
2014-03-28 09:25:10 +0100

    Refactored 'simple' example to be event simpler

--- example/simple.py
+++ example/simple.py
@@ -5,50 +5,36 @@ from pyqp import Column, Table
 from pyqp.reducers import mean
 from pyqp.aggregate import Accumulate
 
-from pyqp.event_dispatcher import Dispatcher
-from pyqp.mappers import dict_row, single_value, values_list, filtered_mapper, Keys
-from pyqp.mappers.filters import aggregate_filter as af2, alias, exclude
-from pyqp.dumpers import csv_dumper, filtered_dumper
-from pyqp.dumpers.filters import aggregate_filter as af, prepend, write_to_stdout
+# create table instance
+table = Table('query_quality', [
+    Column('query_id'),
+    Column('foo'),
+    Column(name='last_succeded'),
+    Column('runtime', mean, Accumulate(60)),
+    Column('success_1h', sum, Accumulate(60)),
+    Column('success_1d', sum, Accumulate(1440))])
 
-def prepare():
-    table = Table('query_quality', [
-        Column('query_id'),
-        Column('foo'),
-        Column(name='last_succeded'),
-        Column('runtime', mean, Accumulate(60)),
-        Column('success_1h', sum, Accumulate(60)),
-        Column('success_1d', sum, Accumulate(1440))])
+# add values
+table.add_value(1, 'query_id', 1)\
+    .add_value(1, 'last_succeded', 2)\
+    .add_value(1, 'runtime', 3)\
+    .add_value(1, 'success_1h', 4)\
+    .add_value(1, 'success_1d', 4)
 
-    dumper = filtered_dumper(csv_dumper, af(prepend('1.23'), write_to_stdout))
-    mapper = filtered_mapper(
-            dict_row('query_id'),
-            af2(
-                alias(success=['success_1h', 'success_1d']),
-                alias(Keys.TABLE_NAME, test=['query_quality']),
-                exclude('success'),
-                exclude('test', key=Keys.TABLE_NAME)
-            )
-        )
-    return (table, dumper, mapper)
+table.add_value(1, 'query_id', 1)\
+    .add_value(1, 'last_succeded', 22)\
+    .add_value(1, 'runtime', 9)\
+    .add_value(1, 'success_1h', 1)\
+    .add_value(1, 'success_1d', 1)
 
-def main(dispatcher):
-    (table, dumper, mapper) = prepare()
-    dispatcher\
-        .add_table(table, dumper)\
-        .add_mapper('pyqp_cell_value', single_value)\
-        .add_mapper('pyqp_cell_list', values_list)\
-        .add_mapper('test', mapper)
+table.add_value(2, 'query_id', 2)\
+    .add_value(2, 'last_succeded', 44)\
+    .add_value(2, 'runtime', 9)\
+    .add_value(2, 'success_1h', 1)\
+    .add_value(2, 'success_1d', 1)
 
-######
-# invoke test
-######
-if '__main__' == __name__:
-    d = Dispatcher()
-    main(d)
-    d.dispatch('test', {'query_id': 1, 'last_succeded': 2, 'runtime': 3, 'success': 4})
-    d.dispatch('test', {'query_id': 1, 'last_succeded': 22, 'runtime': 9, 'success': 1})
-    d.dispatch('test', {'query_id': 2, 'last_succeded': 44, 'runtime': 9, 'success': 1})
-    d.dispatch('pyqp_cell_value', ('query_quality', 2, 'runtime', 1))
-    d.dispatch('pyqp_cell_list', [('query_quality', 1, 'runtime', 4)])
-    d.process()
+table.add_value(1, 'runtime', 4)\
+    .add_value(2, 'runtime', 1)
+
+# dump
+print [[col.reduce() for col in row] for row in table]
