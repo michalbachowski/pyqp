8daee5c0921ad40059e3938e5e53de272695614a
Michał Bachowski <michal@bachowski.pl>
2014-08-07 21:56:22 +0200

    Defined pyqp.version and pyqp.version_info

--- pyqp/__init__.py
+++ pyqp/__init__.py
@@ -2,7 +2,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+# version is a human-readable version number.
 __version__ = '0.1.0'
+version = __version__
+
+# version_info is a four-tuple for programmatic comparison. The first
+# three numbers are the components of the version number.  The fourth
+# is zero for an official release, positive for a development branch,
+# or negative for a release candidate or beta (after the base version
+# number has been incremented)
+version_info = (0, 1, 0, -100)
 
 from pyqp.column import Column
 from pyqp.tables import Table
